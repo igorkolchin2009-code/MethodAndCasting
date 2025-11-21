83164e6e6ce30d8c44209fb919cf824ed737cf8e
Opilane TTHK <opilane@tthk.ee>
2025-11-21 13:17:45 +0200

    lisasin näidise meetodist mis teeb midagi ära, ja meetodist mis tagistab midagi

---
+++ Method/Program.cs
@@ -0,0 +1,41 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        NewMessage();
+        List<string> ostunimekiri = new List<string>();
+        Console.WriteLine("Sisesta oma tänane poeskäigunimekiri");
+        string kasutajaSisestus = "";
+        GetUserInput(kasutajaSisestus, ostunimekiri);
+        foreach (var söök in ostunimekiri)
+        {
+            Console.WriteLine($" -*- {söök}");
+        }
+        GetUserInput(kasutajaSisestus, ostunimekiri);
+    }
+
+    static List<string> GetUserInput(string kasutajasisestus, List<string> ostunimekiri)
+    {
+        while (kasutajasisestus != "rohkem pole")
+            {
+                Console.WriteLine("Kirjuta ükshaaval, sisesta järgmine ost:\nkui rohkem ei ole midagi lisada, siis ütle \"rohkem pole\"");
+                kasutajasisestus = Console.ReadLine();
+                if (kasutajasisestus != "" || kasutajasisestus != "rohkem pole")
+                {
+                    ostunimekiri.Add(kasutajasisestus);
+                }
+                else if (kasutajasisestus == "rohkem pole")
+                {
+                    kasutajasisestus = "";
+                }
+                
+            }
+            Console.WriteLine("see sinu nimekiri");
+        return ostunimekiri;
+    }
+    
+    static void NewMessage()
+    {
+        Console.WriteLine("this is a message");
+    }
+}

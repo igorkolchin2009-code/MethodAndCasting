874a72307d235ed4350c5712badd1b36c8bc9d49
Opilane TTHK <opilane@tthk.ee>
2025-12-05 13:32:40 +0200

    eelarve arvutaja sai valmis

--- Ülesanded/Eelarve Arvutaja/Program.cs
+++ Ülesanded/Eelarve Arvutaja/Program.cs
@@ -4,45 +4,55 @@
     {
         static void Main(string[] args)
         {
+            //küsi kasutajalt tema nime (tsüklis)
             string kasutajaNimi = "";
             while (kasutajaNimi == "")
             {
-                Console.WriteLine("tera, sisesta oma nime;");
+                Console.WriteLine("Tere, sisesta oma nimi:");
                 kasutajaNimi = Console.ReadLine();
-
             }
+
+            //küsi kasutajalt tema tulusid, niikaua kuni rohkem pole
+            //siis küsi kasutajalt tema kulusid, ka niikaua kuni rohkem pole.
+            //hoia andmeid List tüüpi muutujas, andmed ise peavad olema komakohaga.
             string sisestus = "";
-            Console.WriteLine("sisesta tuled ükshaaval, kui siis kirjuta \"rohkem pole \"");
+            Console.WriteLine("Sisesta tulud ükshaaval, kui on kõik, siis kirjuta \"rohkem pole\"");
             List<float> tulud = VõtaKasutajaltMituSisenditJärjest();
             List<float> kulud = VõtaKasutajaltMituSisenditJärjest();
-            
-            
-            if (sisestus == "" );
-            {
-                float kokku = 0;
-                kokku = Arvutiakokku(tulud, kokku);
 
-            }
+            //tee uus meetod, mis arvutab kokku järjendi sisu
+
+            float tuludkokku = 0;
+            tuludkokku = ArvutaKokku(tulud);
+            float kuludkokku = ArvutaKokku(tulud);
+
+            //hoia kogutulusid ja kogukulusid eraldi muutujates kasutades oma meetodit.
+
+            //arvuta kasutajale tulude ja kulude põhjal tema hetkeseis.
+            float kokku = (float)Math.Round((tuludkokku - kuludkokku), 2);
+
+            //Ümarda kasutajale tagastatav arv kahe komakohani. Ja kuva nimeline sõnum
+            Console.WriteLine($" kasutaja: {kasutajaNimi}, teie kontoseis on: {kokku}");
         }
 
-        private static float Arvutiakokku(List<float> tulud, float kokku)
+        private static float ArvutaKokku(List<float> tulud)
         {
-            for (int i = tulud.Count - 1; i >= 0; i--)
+            float kokku = 0;
+            for (int i = 0; i < tulud.Count; i++)
             {
                 kokku += tulud[i];
-
             }
 
             return kokku;
         }
 
-        private static List<float> VõtaKasutajaltMituSisenditJärjest()
+        private static List<float> VõtaKasutajaltMituSisenditJärjest()  
         {
             List<float> tulud = new List<float>();
             string sisestus;
             do
             {
-                Console.WriteLine("sisesta oma andmed *Üksgaaval* - järgmine tulu");
+                Console.WriteLine("Sisesta oma andmed *ÜKSHAAVAL* - järgmine sisestus:");
                 sisestus = Console.ReadLine();
                 if (sisestus == "rohkem pole")
                 {
